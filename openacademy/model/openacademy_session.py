f3c5a77c0a8379168521948f8006b5bacde77d9a
Angelo <angelo_384@hotmail.com>
2016-12-09 20:31:19 +0000

    [RE] openacademy: Computed fields added}

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -1,16 +1,25 @@
 # -*- coding: utf-8 -*-
-from openerp import fields, models
+from openerp import fields, models, api
 
 class Session(models.Model):
-   _name = 'openacademy.session'
+    _name = 'openacademy.session'
 
-   name = fields.Char(required=True)
-   start_date = fields.Date()
-   duration = fields.Float(digits=(6, 2), help="Duration in days")
-   seats = fields.Integer(string="Number of seats")
-   instructor_id = fields.Many2one('res.partner', string="Instructor",
-				domain=['|', ('instructor', '=', True),
-				('category_id.name', 'ilike', "Teacher")])
-   course_id = fields.Many2one('openacademy.course',
-        ondelete='cascade', string="Course", required=True)
-   attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
+    instructor_id = fields.Many2one('res.partner', string="Instructor",
+                                    domain=['|', ('instructor', '=', True),
+                                    ('category_id.name', 'ilike', "Teacher")])
+    course_id = fields.Many2one('openacademy.course',
+    ondelete='cascade', string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+    
+    @api.depends('seats', 'attendee_ids')
+    def _taken_seats(self):
+        for r in self:
+            if not r.seats:
+                r.taken_seats = 0.0
+            else:
+                r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
