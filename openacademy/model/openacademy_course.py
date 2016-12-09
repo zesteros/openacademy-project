bc628f739ce66572777013eeb63c3c55db85960c
Angelo <angelo_384@hotmail.com>
2016-12-09 23:02:49 +0000

    [REF] openacademy: Method copy reimplemented

--- openacademy/model/openacademy_course.py
+++ openacademy/model/openacademy_course.py
@@ -1,4 +1,4 @@
-from openerp import fields, models
+from openerp import api, fields, models
 
 '''
 This module create model of Course
@@ -16,6 +16,22 @@ class Course(models.Model):
                      		     ondelete='set null',
 				     string="Responsible", index=True)
     session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
+
+    @api.multi
+    def copy(self, default=None):
+        default = dict(default or {})
+
+        copied_count = self.search_count(
+                [('name', '=like', u"Copy of {}%".format(self.name))])
+        if not copied_count:
+            new_name = u"Copy of {}".format(self.name)
+        else:
+            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+
+        default['name'] = new_name
+        return super(Course, self).copy(default)
+
+
     _sql_constraints = [
             ('name_description_check',
             'CHECK(name != description)',
