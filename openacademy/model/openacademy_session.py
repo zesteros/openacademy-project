e2520adea074b1a2aad0713ce92a9ef0f1fa3377
Angelo <angelo_384@hotmail.com>
2016-12-09 21:45:01 +0000

    [REF] openacademy: onchanged function added to prevent excess in seats

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
-from openerp import fields, models, api
+from openerp import api, fields, models
 
 class Session(models.Model):
     _name = 'openacademy.session'
 
     name = fields.Char(required=True)
-    start_date = fields.Date()
+    start_date = fields.Date(default=fields.Date.today)
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+    active = fields.Boolean(default=True)
     instructor_id = fields.Many2one('res.partner', string="Instructor",
                                     domain=['|', ('instructor', '=', True),
                                     ('category_id.name', 'ilike', "Teacher")])
@@ -23,3 +24,20 @@ class Session(models.Model):
                 r.taken_seats = 0.0
             else:
                 r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
+    @api.onchange('seats', 'attendee_ids')
+    def _verify_valid_seats(self):
+        if self.seats < 0:
+            return {
+                'warning': {
+                    'title': "Incorrect 'seats' value",
+                    'message': "The number of available seats may not be negative",
+                },
+            }
+        if self.seats < len(self.attendee_ids):
+            return {
+                'warning': {
+                    'title': "Too many attendes",
+                    'message': "Increase seats or remove excess attendees",
+                },
+            }
+
